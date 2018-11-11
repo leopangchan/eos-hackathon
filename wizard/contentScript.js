b7c040bd737d5fc99733df8b559a3db437546db7
Abraham21 <abeyepremian@gmail.com>
2018-11-11 10:55:04 -0800

    added logic to send to blockchain, still needs to be tweaked

--- wizard/contentScript.js
+++ wizard/contentScript.js
@@ -23,28 +23,65 @@ function sendToBlockchain(genre, category, detail) {
   console.log('Data to send to blockchain:', dataArray);
 
   // send to blockchain logic
+  let message = "No response yet";
+  const defaultPrivateKey = "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5"; // useraaaaaaaa
+  const rpc = new eosjs_jsonrpc.default('http://127.0.0.1:8888');
+  const signatureProvider = new eosjs_jssig.default([defaultPrivateKey]);
+  const api = new eosjs_api.default({ rpc, signatureProvider });
+
+  (async () => {
+    try {
+      const result = await api.transact({
+        actions: [{
+          account: 'eosio.token',
+          name: 'transfer',
+          authorization: [{
+            actor: 'useraaaaaaaa',
+            permission: 'active',
+          }],
+          data: {
+            from: 'useraaaaaaaa',
+            to: 'useraaaaaaab',
+            quantity: '0.0001 SYS',
+            memo: '',
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 30,
+      });
+      message += '\n\nTransaction pushed!\n\n' + JSON.stringify(result, null, 2);
+      console.log(message);
+    } catch (e) {
+      message = '\nCaught exception: ' + e;
+      console.log(message);
+      if (e instanceof eosjs_rpcerror.default)
+      message += '\n\n' + JSON.stringify(e.json, null, 2);
+      console.log(message);
+    }
+  })();
 
 }
 
 function isCategoryPath(path) {
   if(path === "/sedan/" || path === "/suv/" || path === "/crossover/" ||
-      path === "/luxury-car/" || path === "/pickup-truck/" || path === "/van-minivan/" ||
-      path === "/hybrid/" || path === "/electric-car/" || path === "/coupe/" ||
-      path === "/hatchback/" || path === "/wagon/" || path === "/convertible") {
-        return true;
-      } else {
-        return false;
-      }
+  path === "/luxury-car/" || path === "/pickup-truck/" || path === "/van-minivan/" ||
+  path === "/hybrid/" || path === "/electric-car/" || path === "/coupe/" ||
+  path === "/hatchback/" || path === "/wagon/" || path === "/convertible") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isCategoryModelPath(path) {
   const slashLength = (path.match(/\//g) || []).length;
   console.log(slashLength);
   if(slashLength === 3) {
-        return true;
-      } else {
-        return false;
-      }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getModel(path) {
