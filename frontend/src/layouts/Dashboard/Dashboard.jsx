3a7c402b63ceedafa45dfcf92b6f663949f70778
leopangchan <leopangchan@gmail.com>
2018-11-11 11:31:31 -0800

    Delete unused icons and update background of slider

--- frontend/src/layouts/Dashboard/Dashboard.jsx
+++ frontend/src/layouts/Dashboard/Dashboard.jsx
@@ -16,7 +16,7 @@ import dashboardRoutes from "routes/dashboard.jsx";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx";
 
-import image from "assets/img/sidebar-2.jpg";
+import image from "assets/img/sf-cable.jpeg";
 import logo from "assets/img/blockies-logo.png";
 
 const switchRoutes = (
