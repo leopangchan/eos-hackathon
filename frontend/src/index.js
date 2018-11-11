09fd761997128bf4188985c6f7d3ef26699ee349
leopangchan <leopangchan@gmail.com>
2018-11-11 00:01:09 -0800

    Integrate ui-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { UIRouter, UIView, pushStateLocationPlugin } from '@uirouter/react';
 import Index from './pages/publisher';
+import User from './pages/user'
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+// define your states
+const states = [
+  {
+    name: 'publisher',
+    url: '/publisher',
+    component: Index,
+  },
+  {
+    name: "user",
+    url: "/user",
+    component: User
+  },
+  {
+    name: "home",
+    url: "/",
+    component: Index
+  }
+];
+
+// select your plugins
+const plugins = [pushStateLocationPlugin];
+
+ReactDOM.render(
+  <UIRouter plugins={plugins} states={states}>
+    <UIView />
+  </UIRouter>,
+  document.getElementById('root'),
+);
