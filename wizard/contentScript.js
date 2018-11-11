533a648a34b521065c6c5956d7303016809326d4
Abraham21 <abeyepremian@gmail.com>
2018-11-11 09:46:19 -0800

    Removed unecessary code

--- wizard/contentScript.js
+++ wizard/contentScript.js
@@ -6,46 +6,6 @@ let localGenre = "Auto";
 let localCategory = "N/A";
 let localDetail = "N/A";
 
-// sets up make listeners after user has clicked on a category
-// currently method not needed
-/* function setupMakes() {
-  console.log('finding current vehicle category...');
-  const category = document.getElementsByClassName("category-name")[0].textContent;
-  console.log('category:', category);
-  console.log('setting makes...');
-  $( ".contentlist.make-list.no-bull.left li" ).each(function( index ) {
-    // fetch make elements from DOM
-    const make = $( this ).text().trim(); // trim keeps white space consistent
-    console.log( index + ": " + make );
-
-    // set on click listener to all makes
-    $( this ).click(function() {
-      console.log('clicked on:', make);
-      sendToBlockchain("Auto", category, make);
-      // send data to eos blockchain
-    });
-  });
-} */
-
-// setups category listeners on https://www.kbb.com/new-cars/
-// currently method not needed
-/* function setupCategories() {
-  console.log('setting categories...');
-  $( ".infotip.category.text-center" ).each(function( index ) {
-    // fetch category elements from DOM
-    const elementContent = $( this ).text().trim(); // trim removes whitespace from start and end
-    const category = elementContent.substr(0,elementContent.indexOf("\n"));
-    console.log( index + ": " + category );
-
-    // set on click listener to all categories
-    $( this ).click(function() {
-      console.log('clicked on:', category);
-      sendToBlockchain("Auto", category);
-      // send data to eos blockchain
-    });
-  });
-} */
-
 function sendToBlockchain(genre, category, detail) {
   // put data into array so I can go through and verify each data field as valid
   let dataArray = [genre, category, detail];
@@ -115,7 +75,6 @@ if(path === "/") {
 } else if(isCategoryPath(path)) {
   getCategory(path);
 } else if(isCategoryModelPath(path)){
-  console.log('is cat model path');
   getCategoryModel(path);
 } else {
   // was model path, get model
